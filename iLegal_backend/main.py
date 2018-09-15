015044ca08cf0d0a36a8ec71656b4487ad88b84b
phil9987 <philipjunker@gmail.com>
2018-09-15 22:10:27 +0200

    fix

--- iLegal_backend/main.py
+++ iLegal_backend/main.py
@@ -5,14 +5,15 @@ from contact import getContactDetails
 import pickle
 app = Flask(__name__)
 
-id2MetaData = pickle.load(open('id2MetaData.pkl','r'))
-nound2Ids = pickle.load(open('noun2Ids.pkl','r'))
-noun2Id = pickle.load(open('noun2Id.pkl','r'))
-allNouns = pickle.load(open('allNouns.pkl','r'))
+id2MetaData = pickle.load(open('id2MetaData.pkl','rb'))
+noun2Ids = pickle.load(open('noun2Ids.pkl','rb'))
+noun2Id = pickle.load(open('noun2Id.pkl','rb'))
+allNouns = pickle.load(open('allNouns.pkl','rb'))
 
 
 @app.route("/")
 def hello():
+    print(allNouns)
     return "iLegal backend V1.1.2"
 
 @app.route('/ai', methods=['POST'])
@@ -28,6 +29,36 @@ def parse_request():
         return jsonify(getContactDetails(hello_name))
     else:
         # todo: do something smart, jaccard similarity of documents etc
+        relevant_nouns = []
+        for noun in nouns:
+            if noun in allNouns:
+                relevant_nouns.append(noun)
+        app.logger.info("Detected the following keywords: ")
+        app.logger.info(relevant_nouns)
+        relevant_cases = {}
+
+        for noun in relevant_nouns:
+            for i in noun2Ids[noun]:
+                relevant_cases[i] = relevant_cases.get(i, 0)
+        similarities = []
+        for i, count in relevant_cases.items():
+            app.logger.info("count:")
+            app.logger.info(count)
+            app.logger.info(len(relevant_nouns))
+            app.logger.info(id2MetaData[i]["numberOfNouns"])
+            similarities.append((i, count / (len(relevant_nouns) + id2MetaData[i]["numberOfNouns"])))
+        
+        sorted(similarities, key=lambda x: x[1], reverse=True)
+        app.logger.info(similarities)
+        i = similarities[0][0]
+        app.logger.info(similarities)
+
+        res = {
+            "text": "This case might be relevant for your current discussion: ",
+            "file_name": id2MetaData[i]["fileName"],
+            "case_title": id2MetaData[i]["name"],
+            "catch_phrases": id2MetaData[i]["abstract"]
+        }
         return jsonify(res)
 
 
